675c0615f0fe409bf974aab8947efaf93a8e61a2
Aziz Azizli <azizliaziz025@gmail.com>
2023-12-17 16:06:32 +0400

    update

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const Card = ({head,body }) => {
+export const Card = ({ head, body }) => {
   return (
-      <div style={{height:"auto",maxHeight:"130px"}} className=' text-white w-4/12   flex flex-col items-center p-2 rounded-lg bg-gray-800'>
-          <p className=' text-xl'>{ head}</p>
-          <p>{ body}</p>
-      </div>
-  )
-}
+    <div
+      style={{ height: "auto", maxHeight: "130px" }}
+      className=" text-white w-4/12   flex flex-col items-center p-2 rounded-lg bg-gray-800"
+    >
+      <p className=" text-xl">{head}</p>
+      <p>{body}</p>
+    </div>
+  );
+};
